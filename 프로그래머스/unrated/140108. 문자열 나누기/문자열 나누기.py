edb5f0843b07a32b3410642834e4ef191abff1a0
Dong-Jae <wlsdk1095sd@gmail.com>
2023-02-06 17:58:42 +0900

    [unrated] Title: 문자열 나누기, Time: 0.72 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
@@ -1,14 +1,12 @@
 def solution(s):
-    ans=0
-    cnt1=0
-    cnt2=0
+    answer = 0
+    cnt1=0;cnt2=0
     for i in s:
         if cnt1==cnt2:
-            ans+=1
+            answer+=1
             k=i
-        if k==i:
+        if i==k:
             cnt1+=1
-        if k!=i:
+        else:
             cnt2+=1
-
-    return ans        
+    return answer

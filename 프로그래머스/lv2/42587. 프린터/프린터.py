00042ed6073c802ad076c7702718454941d7fd1d
Dong-Jae <wlsdk1095sd@gmail.com>
2023-04-08 00:11:27 +0900

    [level 2] Title: 프린터, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.py
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -2,23 +2,18 @@ from collections import deque
 
 def solution(priorities, location):
     answer = 0
-    my_doc = [0 for _ in range(len(priorities))]
-    my_doc[location] = 1
-    queue = deque(priorities)
-    while True:
-
-        if not my_doc or max(my_doc) == 0:
-            print('Break!')
-            break
-        if max(queue) == queue[0]:
-            queue.popleft()
-            del my_doc[0]
-            answer += 1
+    q=deque(priorities)
+    idx=[x for x in range(len(priorities))]
+    
+    while(q):
+        if q[0]==max(q):
+            q.popleft()
+            x=idx.pop(0)
+            answer+=1
+            if x==location:
+                return answer
         else:
-            queue.append(queue[0])
-            my_doc.append(my_doc[0])
-            queue.popleft()
-            del my_doc[0]
-
-
-    return answer
+            p=q.popleft()
+            x=idx.pop(0)
+            q.append(p)
+            idx.append(x)

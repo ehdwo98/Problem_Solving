20fd35cb496ca8db955eff4f38cd21ac52400dab
Dong-Jae <wlsdk1095sd@gmail.com>
2023-04-09 00:29:18 +0900

    Update 다리를 지나는 트럭.py

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,15 +1,16 @@
-from collections import deque
-
 def solution(bridge_length, weight, truck_weights):
-    answer = 0
-    q=deque(truck_weights)
-    while(q):
-        if len(q)<=bridge_length and sum(q)<=weight:
-            return bridge_length+len(q)
-        if len(q)<=bridge_length and sum(q)>weight:
-            
-        if len(q)>bridge_length and sum(q)>weight:
-            p=q.popleft()
-            if weight-p>=q[0] and len(q)>1:
-                answer--
-    return answer
+    time = 0
+    p = list(truck_weights) # 트럭 무게
+    x = [0] * bridge_length # 다리
+    s=0
+    while (x):
+        time += 1
+        s-=x.pop(0) # s: 다리 위의 총 트럭 무게
+        if p:
+            if ( s + p[0] ) <= weight:
+                s+=p[0] # 여유되면 트럭 추가
+                x.append(p.pop(0)) # 다리에 트럭 추가  
+            else:
+                x.append(0) # 여유 없으면 무게 0 추가
+
+    return time

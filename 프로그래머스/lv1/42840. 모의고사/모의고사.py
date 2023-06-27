58df40d849337c098dd7121911003fac171355b1
Dong-Jae <wlsdk1095sd@gmail.com>
2023-06-27 23:05:38 +0900

    [level 1] Title: 모의고사, Time: 2.34 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -2,43 +2,19 @@ def solution(answers):
     answer = []
     n=len(answers)
     cnt1,cnt2,cnt3=0,0,0
-    arr=[1,2,3,4,5]
+    arr1=[1,2,3,4,5]
+    arr2=[2, 1, 2, 3, 2, 4, 2, 5]
+    arr3=[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     for i in range(n):
-        if answers[i]==arr[i%5]:
+        if answers[i]==arr1[i%5]:
             cnt1+=1
-    arr=[2, 1, 2, 3, 2, 4, 2, 5]
-    for i in range(n):
-        if answers[i]==arr[i%8]:
+        if answers[i]==arr2[i%8]:
             cnt2+=1
-    arr=[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    for i in range(n):
-        if answers[i]==arr[i%10]:
+        if answers[i]==arr3[i%10]:
             cnt3+=1
-            
-    if cnt1>cnt2:
-        if cnt1>cnt3:
-            answer.append(1)
-        if cnt1==cnt3:
-            answer.append(1)
-            answer.append(3)
-        if cnt1<cnt3:
-            answer.append(3)
-    if cnt1==cnt2:
-        if cnt2>cnt3:
-            answer.append(1)
-            answer.append(2)
-        if cnt2==cnt3:
-            answer.append(1)
-            answer.append(2)
-            answer.append(3)
-        if cnt2<cnt3:
-            answer.append(3)
-    if cnt1<cnt2:
-        if cnt2>cnt3:
-            answer.append(2)
-        if cnt2==cnt3:
-            answer.append(2)
-            answer.append(3)
-        if cnt2<cnt3:
-            answer.append(3)
+    score=[cnt1,cnt2,cnt3]
+    for idx, s in enumerate(score):
+        if s==max(score):
+            answer.append(idx+1)
+    
     return answer

ca4f8e86d1ad845fcad0d91356df98102cbba51c
Koh, Dong-Jae <wlsdk1095sd@gmail.com>
2024-05-16 00:43:24 +0900

    [Silver II] Title: 유기농 배추, Time: 72 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,30 +1,32 @@
-import sys 
-sys.setrecursionlimit(10000) 
+from collections import deque
+import sys
 
-t=int(input())
+T=int(input())
 
-D=[(-1,0),(1,0),(0,1),(0,-1)]
+D=[(1,0),(-1,0),(0,1),(0,-1)]
 
-def dfs(y,x):
-    for dy,dx in D:
-        ny,nx=y+dy,x+dx
-        if (m>nx>=0) and (n>ny>=0) and arr[ny][nx]==1:
-            arr[ny][nx]=0
-            dfs(ny,nx)
-
-for _ in range(t):
+for _ in range(T):
     m,n,k=map(int,input().split())
-    arr=[[0 for _ in range(m)]for _ in range(n)]
-    res=0
-    
+
+    graph=[[0]*m for _ in range(n)]
+
     for _ in range(k):
-        x,y=map(int,input().split())
-        arr[y][x]=1
-        
-    for a in range(m):
-        for b in range(n):
-            if arr[b][a]==1:
-                dfs(b,a)
-                res+=1
-                
-    print(res)
+        x,y=map(int,sys.stdin.readline().split())
+        graph[y][x]=1
+    
+    q=deque()
+    cnt=0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j]==1:
+                q.append([i,j])
+                while q:
+                    y,x=q.popleft()
+                    for dy,dx in D:
+                        ny,nx=y+dy,x+dx
+                        if 0<=ny<n and 0<=nx<m and graph[ny][nx]==1:
+                            graph[ny][nx]=0
+                            q.append([ny,nx])
+                cnt+=1
+
+    print(cnt)

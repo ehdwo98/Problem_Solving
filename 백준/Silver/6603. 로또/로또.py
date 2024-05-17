9fcca34617e5282da36b3279a5740e7843841625
Koh, Dong-Jae <wlsdk1095sd@gmail.com>
2024-05-17 20:25:27 +0900

    [Silver II] Title: 로또, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -7,9 +7,21 @@ while 1:
     if n==0:
         break
     arr2=arr[1::]
+
+    arr3=[]
+    isused=[0]*n
+
+    def lotto(idx):
+        if len(arr3)==6:
+            print(*arr3)
+            return
+        for i in range(idx,len(arr2)):
+            if isused[i]==0:
+                arr3.append(arr2[i])
+                isused[i]=1
+                lotto(i+1)
+                isused[i]=0
+                arr3.pop()
     
-    combi=list(combinations(arr2,6))
-    
-    for c in combi:
-        print(*c)
+    lotto(0)
     print()

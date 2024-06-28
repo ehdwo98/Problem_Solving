bdee47e7e5e07f441b65b17c007a097b76b40d94
Koh DongJae <wlsdk1095sd@gmail.com>
2024-06-29 03:06:05 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 56 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
@@ -2,13 +2,15 @@ from collections import deque
 n,k=map(int,input().split())
 
 arr=[x for x in range(2,n+1)]
+visited=[0]*(n+1)
 q=deque(arr)
 while q:
     p=q[0]
     for i in range(1,n//p+1):
-        if p*i in q:
+        if visited[p*i]==0:
             k-=1
             if k==0:
                 print(p*i)
                 break
             q.remove(p*i)
+            visited[p*i]=1

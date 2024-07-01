f6e3af8ba0aaedc3c6a150b6f5510455c6ade2b3
Koh DongJae <wlsdk1095sd@gmail.com>
2024-07-01 15:53:29 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 392 ms, Memory: 67900 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -4,8 +4,10 @@ graph=[[' ']*n for _ in range(n)]
 #print(graph)
 
 def recur(n,r,c):
-    if n==1:
-        graph[r][c]='*'
+    if n==3:
+        for i in range(3):
+            graph[r][c+i]=graph[r+2][c+i]='*'
+        graph[r+1][c]=graph[r+1][c+2]='*'
         return
     recur(n//3,r,c)
     recur(n//3,r,c+n//3)

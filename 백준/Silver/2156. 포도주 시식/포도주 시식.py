1c1a53aecfa138d137dda490f634140bed611791
Koh DongJae <wlsdk1095sd@gmail.com>
2024-07-20 20:18:33 +0900

    [Silver I] Title: 포도주 시식, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+arr=[0]+list(int(input()) for _ in range(n))
+
+dp=[0]*(n+1)
+dp[1]=arr[1]
+if n>1:
+    dp[2]=arr[1]+arr[2]
+    for i in range(3,n+1):
+        dp[i]=max(dp[i-1],dp[i-2]+arr[i],dp[i-3]+arr[i-1]+arr[i])
+
+print(dp[n])

be14615f675c3d48bcbb6e53152054503e039c49
Koh DongJae <wlsdk1095sd@gmail.com>
2024-07-20 20:53:10 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 432 ms, Memory: 70676 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+
+t=int(input())
+
+dp=[0]*(1000001)
+dp[1]=1
+dp[2]=2
+dp[3]=1+dp[1]+dp[2]
+
+for i in range(4,1000001):
+    dp[i]=(dp[i-3]+dp[i-2]+dp[i-1])%1000000009
+
+for i in range(t):
+    n=int(input())
+    print(dp[n]%1000000009)

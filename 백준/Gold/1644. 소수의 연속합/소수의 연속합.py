ac5d026d851c71951af1d02752573dc6546eb6c5
Dong Jae <wlsdk1095sd@gmail.com>
2024-08-27 02:41:53 +0900

    [Gold III] Title: 소수의 연속합, Time: 1304 ms, Memory: 93620 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -1,12 +1,14 @@
 n=int(input())
 
 is_prime=[0,0]+[1]*(n-1)
+for i in range(2,int(n**0.5)+1):
+   if is_prime[i]:
+      for j in range(i*2,n+1,i):
+         is_prime[j]=0
 arr=[]
 for i in range(2,n+1):
    if is_prime[i]:
       arr.append(i)
-      for j in range(i*2,n+1,i):
-         is_prime[j]=0
 
 arr_n=len(arr)
 

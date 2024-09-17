9531be3fd5f351ee217051b0bfe6605c4c2a13f4
choco granola <wlsdk1095sd@gmail.com>
2024-09-18 05:45:28 +0900

    [Silver IV] Title: 대칭 차집합, Time: 216 ms, Memory: 81684 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -4,10 +4,6 @@ input=sys.stdin.readline
 a,b=map(int,input().split())
 
 arr=set(map(int,input().split()))
-brr=set(map(int,input().split()))
+arr^=set(map(int,input().split()))
 
-ab=list(arr-brr)
-ba=list(brr-arr)
-
-# print(ab,ba)
-print(len(ab)+len(ba))
+print(len(arr))

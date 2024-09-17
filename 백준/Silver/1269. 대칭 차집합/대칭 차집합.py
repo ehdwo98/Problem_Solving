a3e34c04fa380e5fe7d12474c030754c80be565d
choco granola <wlsdk1095sd@gmail.com>
2024-09-18 05:46:12 +0900

    [Silver IV] Title: 대칭 차집합, Time: 200 ms, Memory: 81684 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -5,5 +5,7 @@ a,b=map(int,input().split())
 
 arr=set(map(int,input().split()))
 arr^=set(map(int,input().split()))
+# print(set.difference(a,b), a-b) #차집합
+# print(set.symmetric_difference(a,b), a^b) #대칭차집합
 
 print(len(arr))

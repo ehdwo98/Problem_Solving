7c782f09f8cad484504f09757f96d966414bfef3
choco granola <wlsdk1095sd@gmail.com>
2024-10-21 02:33:42 +0900

    [Silver II] Title: 섬의 개수, Time: 68 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,7 +1,6 @@
 from collections import deque
 
 D=[(1,0),(0,1),(-1,0),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]
-D2=[1,-1]
 
 def bfs(h,w):
     ans=0
@@ -20,32 +19,9 @@ def bfs(h,w):
                 ans+=1
     print(ans)
 
-def bfs2(h):
-    ans=0
-    for i in range(h):
-        if graph[i]:
-            q=deque()
-            q.append(i)
-            while q:
-                x=q.popleft()
-                for dx in D2:
-                    nx=x+dx
-                    if 0<=nx<h and graph[nx]:
-                        graph[nx]=0
-                        q.append(nx)
-            ans+=1
-    print(ans)
-
 while 1:
     w,h=map(int,input().split())
     if (w,h)==(0,0):
         break
-    if w==1:
-        graph=list(int(input()) for _ in range(h))
-        bfs2(h)
-    elif h==1:
-        graph=list(map(int,input().split()))
-        bfs2(w)
-    else:
-        graph=list(list(map(int,input().split())) for _ in range(h))
-        bfs(h,w)
+    graph=list(list(map(int,input().split())) for _ in range(h))
+    bfs(h,w)

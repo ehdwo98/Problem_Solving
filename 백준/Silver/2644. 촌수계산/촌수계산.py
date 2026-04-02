af3e90decb59b142cca548a87b0a7e34a2264708
Dong-Jae Koh <wlsdk1095sd@gmail.com>
2026-04-02 20:12:22 +0900

    [Silver II] Title: 촌수계산, Time: 56 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,41 +1,20 @@
-n=int(input())#전체 사람 수
-a,b=map(int,input().split())#촌수 계산해야하는 서로 다른 두 사람 번호
-m=int(input())#부모 자식들 관계 개수
+from collections import deque
 
-parent=[0]*(n+1)
+n=int(input())
+a,b=map(int,input().split())
+m=int(input())
+parent=list(list() for _ in range(n+1))
 for _ in range(m):
     x,y=map(int,input().split())
-    parent[y]=x
-
-d1=dict()
-cur=a
-cnt=0
-while cur!=0:
-    d1[cur]=cnt
-    cur=parent[cur]
-    cnt+=1
-# print(d1)
-d2=dict()
-cur=b
-cnt=0
-while cur!=0:
-    if cur in d1:
-        # print(d2)
-        print(d1[cur]+cnt)
-        exit(0)
-    d2[cur]=cnt
-    cur=parent[cur]
-    cnt+=1
-# print(d2)
-print(-1)
-
-# 9
-# 2 7
-# 7
-# 1 2
-# 1 3
-# 2 7
-# 2 8
-# 2 9
-# 4 5
-# 4 6
+    parent[x].append(y)
+    parent[y].append(x)
+visited=[-1]*(n+1)
+visited[a]=0
+q=deque([a])
+while q:
+    p=q.popleft()
+    for nxt in parent[p]:
+        if visited[nxt]==-1:
+            visited[nxt]=visited[p]+1
+            q.append(nxt)
+print(visited[b])

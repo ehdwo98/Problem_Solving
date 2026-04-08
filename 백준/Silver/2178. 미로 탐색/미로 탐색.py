1de641b3ab0e4daea46b2e8328406ba3ac780ebb
Dong-Jae Koh <wlsdk1095sd@gmail.com>
2026-04-08 17:34:50 +0900

    [Silver I] Title: 미로 탐색, Time: 88 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,25 +1,18 @@
+# 최소의 칸 수 -> BFS deque사용
 from collections import deque
-
 n,m=map(int,input().split())
-
-graph=[]
-for _ in range(n):
-    graph.append(list(map(int,input())))
-
-D=[(1,0),(0,1),(-1,0),(0,-1)]
-
-def miro():
-    q=deque()
-    q.append([0,0])
-    while q:
-        x,y=q.popleft()
-        for dx,dy in D:
-            nx,ny=x+dx,y+dy
-            if 0<=nx<n and 0<=ny<m:
-                if graph[nx][ny]==1:
-                    q.append([nx,ny])
-                    graph[nx][ny]+=graph[x][y]
-                    if [nx,ny]==[n-1,m-1]:
-                        return graph[nx][ny]
-ans=miro()
-print(ans)
+graph=list(list(map(int,list(input()))) for _ in range(n))
+visited=list([0]*m for _ in range(n))
+# print(graph)
+q=deque()
+q.append([0,0])
+visited[0][0]=1 # 1,1 칸
+D=[(0,1),(0,-1),(1,0),(-1,0)]#서로 인전한 칸
+while q:
+    x,y=q.popleft()
+    for dx,dy in D:
+        nx,ny=x+dx,y+dy
+        if 0<=nx<n and 0<=ny<m and graph[nx][ny] and not visited[nx][ny]:
+            visited[nx][ny]=visited[x][y]+1
+            q.append([nx,ny])
+print(visited[n-1][m-1])

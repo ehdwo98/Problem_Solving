8098880a2c6e26c465fc6c84b6a0b5c54a6dde8b
Dong-Jae Koh <wlsdk1095sd@gmail.com>
2026-04-16 01:56:38 +0900

    [Gold V] Title: 토마토, Time: 3632 ms, Memory: 53380 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,43 +1,36 @@
+#토마토가 모두 익을때가지의 최소 시간 -> BFS탐색
 from collections import deque
 
 m,n,h=map(int,input().split())
 
-graph=[[list(map(int,input().split())) for _ in range(n)] for _ in range(h)]
-day = [[[0] * m for _ in range(n)] for _ in range(h)]
-visited = [[[0] * m for _ in range(n)] for _ in range(h)]
+tomatos=list(list(list(map(int,input().split())) for _ in range(n)) for _ in range(h))
+# print(tomatos)
 
+#여섯 방향
+D=[(1,0,0),(-1,0,0),(0,1,0),(0,-1,0),(0,0,1),(0,0,-1)]
+
+#처음부터 익어있는 모든 토마토를 한 큐에 동시에 넣고 퍼뜨리기
 q=deque()
-D=[(0,1,0),(0,-1,0),(0,0,1),(0,0,-1),(1,0,0),(-1,0,0)]
-def bfs():
-    while q:
-        x,y,z=q.popleft()
-        visited[x][y][z]=1
-        for dx,dy,dz in D:
-               nx,ny,nz=x+dx,y+dy,z+dz
-               if 0<=nx<h and 0<=ny<n and 0<=nz<m:
-                    if visited[nx][ny][nz]==0 and graph[nx][ny][nz]==0:
-                        day[nx][ny][nz]=day[x][y][z]+1
-                        visited[nx][ny][nz]=1
-                        q.append([nx,ny,nz])
+for a in range(h):
+    for b in range(n):
+        for c in range(m):
+            if tomatos[a][b][c]==1:
+                q.append([a,b,c])
 
-for k in range(h):
-    for i in range(n):
-        for j in range(m):
-            if graph[k][i][j]==-1:
-                visited[k][i][j]=1
-            if graph[k][i][j]==1:
-                q.append([k,i,j])
-    
-bfs()
-    
-max=0
-for k in range(h):
-    for i in range(n):
-        for j in range(m):
-            if visited[k][i][j]==0:
-                print(-1)
-                exit(0)
-            if day[k][i][j]>max:
-                max=day[k][i][j]
+while q:
+    x,y,z=q.popleft()
+    for dx,dy,dz in D:
+        nx,ny,nz=x+dx,y+dy,z+dz
+        if 0<=nx<h and 0<=ny<n and 0<=nz<m and not tomatos[nx][ny][nz]:
+            tomatos[nx][ny][nz]=tomatos[x][y][z]+1
+            q.append([nx,ny,nz])
 
-print(max)
+# print(tomatos)
+day=1
+for tomato in tomatos:
+    for row in tomato:
+        if 0 in row:
+            print(-1)
+            exit(0)
+        day=max(day,max(row))
+print(day-1)

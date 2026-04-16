40656118b58bad07986e06183b51e9c672f9d459
Dong-Jae Koh <wlsdk1095sd@gmail.com>
2026-04-16 19:08:35 +0900

    [Gold V] Title: 토마토, Time: 3660 ms, Memory: 53376 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,34 +1,29 @@
-#토마토가 모두 익을때가지의 최소 시간 -> BFS탐색
+#토마토토가 모두 익을때까지 최소 며칠 걸리는지 계산 -> BFS탐색
 from collections import deque
-
 m,n,h=map(int,input().split())
+graph=list(list(list(map(int,input().split())) for _ in range(n)) for _ in range(h))
+# print(graph)
 
-tomatos=list(list(list(map(int,input().split())) for _ in range(n)) for _ in range(h))
-# print(tomatos)
-
-#여섯 방향
-D=[(1,0,0),(-1,0,0),(0,1,0),(0,-1,0),(0,0,1),(0,0,-1)]
-
-#처음부터 익어있는 모든 토마토를 한 큐에 동시에 넣고 퍼뜨리기
 q=deque()
-for a in range(h):
-    for b in range(n):
-        for c in range(m):
-            if tomatos[a][b][c]==1:
-                q.append([a,b,c])
+for i in range(h):
+    for j in range(n):
+        for k in range(m):
+            if graph[i][j][k]==1:
+                q.append([i,j,k])
+
+D=[(1,0,0),(-1,0,0),(0,1,0),(0,-1,0),(0,0,1),(0,0,-1)]
 
 while q:
     x,y,z=q.popleft()
     for dx,dy,dz in D:
         nx,ny,nz=x+dx,y+dy,z+dz
-        if 0<=nx<h and 0<=ny<n and 0<=nz<m and not tomatos[nx][ny][nz]:
-            tomatos[nx][ny][nz]=tomatos[x][y][z]+1
+        if 0<=nx<h and 0<=ny<n and 0<=nz<m and graph[nx][ny][nz]==0:
+            graph[nx][ny][nz]=graph[x][y][z]+1
             q.append([nx,ny,nz])
 
-# print(tomatos)
 day=1
-for tomato in tomatos:
-    for row in tomato:
+for g in graph:
+    for row in g:
         if 0 in row:
             print(-1)
             exit(0)

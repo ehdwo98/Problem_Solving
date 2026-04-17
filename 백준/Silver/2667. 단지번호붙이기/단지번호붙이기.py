05a34a42200eb89a1d6a72ef1d14bd329bf22563
Dong-Jae Koh <wlsdk1095sd@gmail.com>
2026-04-18 02:28:46 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,30 +1,30 @@
-N=int(input())
+#단지 탐색 -> BFS탐색
+from collections import deque
 
-maps=[list(map(int,input())) for _ in range(N)]
-visit=[[0]*N for _ in range(N)]
+n=int(input())
+graph=list(list(map(int,input())) for _ in range(n))
+# print(graph)
+visited=list([0]*n for _ in range(n))
+D=[(1,0),(-1,0),(0,1),(0,-1)]
 
-D=[[1,0],[-1,0],[0,1],[0,-1]]
+ans=list()
+for i in range(n):
+    for j in range(n):
+        if graph[i][j]==1 and not visited[i][j]:
+            q=deque()
+            visited[i][j]=1
+            q.append([i,j])
+            cnt=1
+            while q:
+                x,y=q.popleft()
+                for dx,dy in D:
+                    nx,ny=x+dx,y+dy
+                    if 0<=nx<n and 0<=ny<n and graph[nx][ny] and not visited[nx][ny]:
+                        cnt+=1
+                        visited[nx][ny]=1
+                        q.append([nx,ny])
+            ans.append(cnt)
 
-num=[]
-cnt=0
-def dfs(x,y):
-    if 0<=x<N and 0<=y<N and maps[x][y]==1:
-        global cnt
-        cnt+=1
-        maps[x][y]=0
-        for i,j in D:
-            nx,ny=x+i,y+j
-            dfs(nx,ny)
-        return True
-    return False
-    
-for i in range(N):
-    for j in range(N):
-        if dfs(i,j)==True:
-            num.append(cnt)
-            cnt=0
-
-num.sort()
-print(len(num))
-for i in num:
-    print(i)
+print(len(ans))
+for a in sorted(ans):
+    print(a)

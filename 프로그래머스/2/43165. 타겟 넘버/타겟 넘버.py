2418afe05683cd7fe262d6e7eaad4d1394119fdb
Dong-Jae Koh <wlsdk1095sd@gmail.com>
2026-04-21 19:43:26 +0900

    [level 2] Title: 타겟 넘버, Time: 693.85 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,13 +1,16 @@
+ans=0
+def dfs(numbers,target,cnt):
+    global ans
+    # print(numbers)
+    if cnt==len(numbers):
+        if sum(numbers)==target:
+            ans+=1
+        return
+    dfs(numbers,target,cnt+1)
+    numbers[cnt]=-numbers[cnt]
+    dfs(numbers,target,cnt+1)
+
 def solution(numbers, target):
     answer = 0
-    def dfs(x,n):
-        if n==len(numbers):
-            if x==target:
-                return 1
-            else:
-                return 0
-        return dfs(x+numbers[n],n+1)+dfs(x-numbers[n],n+1)
-        
-    answer=dfs(0,0)
-
-    return answer
+    dfs(numbers,target,0)
+    return ans

3552a16d6d58db8aef3b09bd3a28fe980d3e2c4a
Dong-Jae Koh <wlsdk1095sd@gmail.com>
2026-06-16 17:08:55 +0900

    [level 1] Title: 폰켓몬, Time: 1.39 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -5,6 +5,8 @@ def solution(nums):
         if num not in dic:
             dic[num]=0
         dic[num]+=1
-    l=len(list(dic.items()))
-    answer=l if l<len(nums)//2 else len(nums)//2
-    return answer
+    l=len(dic.keys())
+    if l>len(nums)//2:
+        return len(nums)//2
+    else:
+        return l

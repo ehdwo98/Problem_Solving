77fbe82e60d544740f6453d60119d184b2b3c334
Dong-Jae Koh <wlsdk1095sd@gmail.com>
2026-06-30 14:07:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.66 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,23 +1,22 @@
-# 도착하기 위해서 지나가야 하는 칸의 최소 갯수 -> BFS탐색
 from collections import deque
 
 def solution(maps):
-    answer = -1
+    answer = 0
     n=len(maps)
     m=len(maps[0])
+    D=[(1,0),(-1,0),(0,1),(0,-1)]
     visited=list([0]*m for _ in range(n))
     q=deque()
-    visited[0][0]=1
     q.append([0,0])
-    D=[(1,0),(-1,0),(0,1),(0,-1)]
+    visited[0][0]=1
     while q:
         x,y=q.popleft()
         for dx,dy in D:
             nx,ny=x+dx,y+dy
-            if 0<=nx<n and 0<=ny<m and maps[nx][ny] and not visited[nx][ny]:
-                visited[nx][ny]=visited[x][y]+1
-                if (nx,ny)==(n-1,m-1):
-                    answer=visited[nx][ny]
-                    break
+            if 0<=nx<n and 0<=ny<m and not visited[nx][ny] and maps[nx][ny]:
                 q.append([nx,ny])
+                maps[nx][ny]=maps[x][y]+1
+                visited[nx][ny]=1
+    answer=maps[n-1][m-1]
+    if answer==1: answer=-1
     return answer

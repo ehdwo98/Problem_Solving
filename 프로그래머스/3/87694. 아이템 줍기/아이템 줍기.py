03b5fcea82fd0f024fa80e53896e56d869e1cab9
Dong-Jae Koh <wlsdk1095sd@gmail.com>
2026-07-08 11:33:02 +0900

    [level 3] Title: 아이템 줍기, Time: 0.64 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,31 +1,36 @@
-#최단거리 -> BFS탐색
 from collections import deque
 
 def solution(rectangle, characterX, characterY, itemX, itemY):
     answer = 0
-    n=101
-    grid=list([0]*n for _ in range(n))
-    visited=list([0]*n for _ in range(n))
+    graph=list([0]*101 for _ in range(101))
+    
+    x,y=characterX*2,characterY*2
     for lx,ly,rx,ry in rectangle:
         lx,ly,rx,ry=lx*2,ly*2,rx*2,ry*2
-        print(lx,ly,rx,ry)
         for i in range(lx,rx+1):
             for j in range(ly,ry+1):
-                if lx<i<rx and ly<j<ry:
-                    grid[i][j]=2#내부
-                elif grid[i][j]!=2:
-                    grid[i][j]=1#선
-    sx,sy,ex,ey=characterX*2,characterY*2,itemX*2,itemY*2
+                if i==lx or i==rx or j==ly or j==ry:
+                    if graph[i][j]!=2:
+                        graph[i][j]=1#테두리
+                else:
+                    graph[i][j]=2#내부
+    # print(graph)
+    visited=list([0]*101 for _ in range(101))
+    D=[(1,0),(-1,0),(0,1),(0,-1)]
+    
     q=deque()
-    visited[sx][sy]=1
-    q.append([sx,sy,0])
+    q.append([x,y])
+    visited[x][y]=1
     while q:
-        x,y,d=q.popleft()
-        if x==ex and y==ey:
-            answer=d//2
-        for dx,dy in [(1,0),(-1,0),(0,1),(0,-1)]:
-            nx,ny=x+dx,y+dy
-            if 0<nx<n and 0<ny<n and grid[nx][ny]==1 and not visited[nx][ny]:
-                visited[nx][ny]=1
-                q.append([nx,ny,d+1])
+        a,b=q.popleft()
+        if a==itemX*2 and b==itemY*2:
+            answer=visited[a][b]//2
+            break
+        for da,db in D:
+            na,nb=a+da,b+db
+            if 0<=na<101 and 0<=nb<101:
+                if graph[na][nb]==1 and not visited[na][nb]:
+                    q.append([na,nb])
+                    visited[na][nb]=visited[a][b]+1
+    
     return answer

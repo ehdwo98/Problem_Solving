73f0bda823282952aa9781cedbcd71aa38ce5296
Dong-Jae Koh <wlsdk1095sd@gmail.com>
2026-09-02 16:27:52 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.15 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 11.2 MB, 시간: 5.66 ms
+메모리: 11.3 MB, 시간: 6.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 30일 14:07:35
+2026년 09월 02일 16:27:45
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,22 +1,24 @@
 from collections import deque
 
 def solution(maps):
-    answer = 0
+    answer = -1
     n=len(maps)
     m=len(maps[0])
-    D=[(1,0),(-1,0),(0,1),(0,-1)]
     visited=list([0]*m for _ in range(n))
     q=deque()
     q.append([0,0])
     visited[0][0]=1
     while q:
         x,y=q.popleft()
-        for dx,dy in D:
+        visited[x][y]=1
+        for dx,dy in [(1,0),(0,1),(-1,0),(0,-1)]:
             nx,ny=x+dx,y+dy
-            if 0<=nx<n and 0<=ny<m and not visited[nx][ny] and maps[nx][ny]:
-                q.append([nx,ny])
-                maps[nx][ny]=maps[x][y]+1
-                visited[nx][ny]=1
-    answer=maps[n-1][m-1]
-    if answer==1: answer=-1
+            if 0<=nx<n and 0<=ny<m and not visited[nx][ny]:
+                if maps[nx][ny]==1:
+                    maps[nx][ny]=maps[x][y]+1
+                    q.append([nx,ny])
+                    visited[nx][ny]=1
+                    if nx==n-1 and ny==m-1:
+                        answer=maps[nx][ny]
+                        return answer
     return answer
